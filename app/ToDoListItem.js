c616a682c24dde5e7c882b68f2873ae6ed769ba6
ahonn <ahonn95@outlook.com>
2016-03-10 22:02:42 +0800

    change List Item style

--- app/ToDoListItem.js
+++ app/ToDoListItem.js
@@ -5,53 +5,19 @@ import React,{
     StyleSheet,
     TouchableHighlight
 } from 'react-native';
+import { Button } from 'react-native-material-design';
 
 export default class ToDoListItem extends React.Component {
   render() {
     var item = this.props.item;
     return (
       <View>
-        <TouchableHighlight
-          onPress={this.props.onPress}
-          onLongPress={this.props.onLongPress}>
-          <View style={styles.container}>
-              <Text style={[styles.txt, item.complete && styles.completed]}>
-                  {item.txt}
-              </Text>
-          </View>
-        </TouchableHighlight>
-        <View style={styles.hr}/>
+      <Card>
+          <Card.Body>
+              <Text>{item.txt}</Text>
+          </Card.Body>
+      </Card>
       </View>
     );
   }
 }
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    padding: 15,
-    backgroundColor: '#ffffff',
-  },
-
-  txt: {
-    fontSize: 20,
-    marginLeft: 5,
-    marginTop: 2,
-    color: '#222222',
-  },
-
-  completed: {
-    color: '#cccccc'
-  },
-
-  hr: {
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    height: 1,
-    marginLeft: 0,
-    marginRight: 0,
-  }
-
-});
